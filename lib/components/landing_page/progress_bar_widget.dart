c2bea16f149530ba16b7da2403f8a6db9084c222
RedFlameKen <kabalacaba@gmail.com>
2025-10-30 11:28:53 +0800

    fix(583a531): make ProgressBarWidget a StatefulWidget progress bar needs to be a stateful widget because it has a value that needs to change

--- lib/components/landing_page/progress_bar_widget.dart
+++ lib/components/landing_page/progress_bar_widget.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:chiwi/style/colors.dart';
 
-class ProgressBarWidget extends StatelessWidget {
+class ProgressBarWidget extends StatefulWidget {
+  const ProgressBarWidget({super.key});
+
+  @override
+  State<ProgressBarWidget> createState() => _ProgressBarWidgetState();
+}
+
+class _ProgressBarWidgetState extends State<ProgressBarWidget> {
+  double _value = 0;
+
+  void setValue(double value) {
+    setState(() {
+      _value = value;
+    });
+  }
+
+  void incrementValue() {
+    setState(() {
+      _value += 0.1;
+    });
+  }
+
+  double getValue() {
+    return _value;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -11,11 +36,12 @@ class ProgressBarWidget extends StatelessWidget {
           width: 20,
           child: LinearProgressIndicator(
             //value: (_currentQuestionIndex + 1) / questions.length,
+            value: _value,
             backgroundColor: ChiwiColors.MATCHA,
             valueColor: AlwaysStoppedAnimation<Color>(ChiwiColors.CHAI),
           ),
         ),
-      ]
+      ],
     );
   }
 }
