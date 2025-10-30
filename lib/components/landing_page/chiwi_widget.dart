2a55e1f4f77a54da73313a6c7aff52c9af46d2b6
RedFlameKen <kabalacaba@gmail.com>
2025-10-30 11:15:32 +0800

    fix(4343058): Make ChiwiWidget actually return the image instead of the empty scaffold

--- lib/components/landing_page/chiwi_widget.dart
+++ lib/components/landing_page/chiwi_widget.dart
@@ -2,14 +2,9 @@ import 'package:flutter/material.dart';
 
 class ChiwiWidget extends StatelessWidget {
   const ChiwiWidget({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    Image(
-      image: NetworkImage("https://i.imgflip.com/77e8vi.png")
-      );
-      return Scaffold();
+    return Image(image: NetworkImage("https://i.imgflip.com/77e8vi.png"));
   }
-
-  
 }
