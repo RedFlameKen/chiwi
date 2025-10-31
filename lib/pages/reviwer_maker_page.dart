218c4935413e1d17dc6f550938a7b842c640de52
RedFlameKen <nettolacaba@gmail.com>
2025-11-01 00:24:04 +0800

    fix: make reviewer output box fill all available space

--- lib/pages/reviwer_maker_page.dart
+++ lib/pages/reviwer_maker_page.dart
@@ -6,61 +6,62 @@ class ReviwermakerPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
     final half = screenWidth / 2;
-   
+
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(color: ChiwiColors.ALMOND),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children:[
-              Container(//this is the green widget 
-                width: half,
-                height: screenHeight,
-                color: ChiwiColors.MATCHA,
-                child: Column(
-                  children:[
-                    Expanded(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              //this is the green widget
+              width: half,
+              color: ChiwiColors.MATCHA,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      width: double.infinity,
                       child: Container(
                         margin: EdgeInsets.all(8),
                         padding: EdgeInsets.all(12),
                         decoration: BoxDecoration(
                           color: ChiwiColors.ALMOND,
-                          borderRadius: BorderRadius.circular(5)
+                          borderRadius: BorderRadius.circular(5),
                         ),
-                        child: Text(//text display
+                        child: Text(
+                          //text display
                           'this is to display the inputs and response',
                           style: TextStyle(fontSize: 20),
                         ),
                       ),
                     ),
-                    Spacer(),
-                    Container(
-                      margin: EdgeInsets.all(8),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'This is a test...',
-                          filled: true,
-                          fillColor: ChiwiColors.ALMOND,
-                          contentPadding: EdgeInsets.symmetric(
-                            vertical: 15,
-                            horizontal: 10
-                          ),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5)
-                          )
-                          )
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(8),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'This is a test...',
+                        filled: true,
+                        fillColor: ChiwiColors.ALMOND,
+                        contentPadding: EdgeInsets.symmetric(
+                          vertical: 15,
+                          horizontal: 10,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
                         ),
                       ),
-                  ], 
-                )
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-     );
+      ),
+    );
   }
 }
