2b3941eea3abdb74b34ed2ed53d9e0afc3317834
RedFlameKen <nettolacaba@gmail.com>
2025-11-01 19:51:08 +0800

    fix: create form and its validations

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,54 +1,21 @@
+import 'package:chiwi/components/form/signup_form.dart';
+import 'package:chiwi/components/input/button.dart';
 import 'package:chiwi/components/chiwi/chiwi_widget.dart';
+import 'package:chiwi/components/input/text_input.dart';
 import 'package:chiwi/pages/templates/two_section_page.dart';
 import 'package:flutter/material.dart';
 
 class SignupPage extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() => _SignupPageState();
-
 }
 
 class _SignupPageState extends State<SignupPage> {
-
   @override
   Widget build(BuildContext context) {
     return TwoSectionPage(
-      leftChild: Column(
-        children: [
-          Row(
-            children: [
-            Spacer(),
-            Expanded(
-              flex: 2,
-              child: Text(
-                "Create a Username and Password",
-                textAlign: .center,
-                style: TextStyle(fontWeight: .bold, fontSize: 36),
-                ),
-            ),
-            Spacer(),
-            ],
-          ),
-          Column(
-            mainAxisSize: .max,
-            mainAxisAlignment: .spaceEvenly,
-            children: [
-              TextField(),
-              TextField(),
-              TextField()
-            ],
-          ),
-          Container(
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text("Create Account"),
-            )
-          )
-        ],
-      ),
-      rightChild: ChiwiWidget()
+      leftChild: SignupForm(),
+      rightChild: ChiwiWidget(),
     );
   }
-
 }
