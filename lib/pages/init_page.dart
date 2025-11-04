251fed84fa6951ca18458632c9105a5ba72a176f
RedFlameKen <nettolacaba@gmail.com>
2025-11-04 22:38:51 +0800

    feat: enable automatic login when the app starts To make it less an annoying, implement a system to automatically log the user in when the web app starts. flutter_secure_storage has been used to persist the login info of the user (username and auth_token) whenever loginWithUsernameAndPassword() or loginWithAuthToken() are used.

---
+++ lib/pages/init_page.dart
@@ -0,0 +1,61 @@
+import 'package:chiwi/auth/account.dart';
+import 'package:chiwi/pages/landing_page.dart';
+import 'package:chiwi/pages/main_menu_page.dart';
+import 'package:chiwi/style/colors.dart';
+import 'package:flutter/material.dart';
+
+class InitPage extends StatefulWidget {
+  InitPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _InitPageState();
+}
+
+class _InitPageState extends State<InitPage> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _init();
+    });
+  }
+
+  Future<void> _init() async {
+    await AccountManager.INSTANCE.loginWithAuthToken(
+      onSuccess: (message) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text(message ?? "logged in")));
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => MainMenuPage()),
+        );
+      },
+      onFail: (message) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text(message ?? "failed to login")));
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (_) {
+              return LandingPage();
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: ChiwiColors.VANILLA,
+        child: Center(
+          child: CircularProgressIndicator(color: ChiwiColors.MATCHA),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -3,7 +3,7 @@ import 'package:chiwi/style/colors.dart';
 import 'package:flutter/material.dart';
 
 class LandingPage extends StatelessWidget {
-  const LandingPage({super.key});
+  LandingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
