5945f6c983f469008d93a7b00b90605d201bc517
RedFlameKen <nettolacaba@gmail.com>
2025-11-17 08:40:56 +0800

    fix: move scaffold to the page widget

--- lib/components/dashboard/crud_widget.dart
+++ lib/components/dashboard/crud_widget.dart
@@ -30,7 +30,9 @@ class _ReviewerDashboard extends State<ReviewerDashboard> {
   }
 
   Future<void> searchReviews() async {
-    final response = await http.get(Uri.parse('$searchTerm')); //for searching for reviews
+    final response = await http.get(
+      Uri.parse('$searchTerm'),
+    ); //for searching for reviews
     if (response.statusCode == 200) {
       setState(() {
         getReviews = json.decode(response.body);
@@ -42,71 +44,69 @@ class _ReviewerDashboard extends State<ReviewerDashboard> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Reviewers'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              searchTerm = _searchController.text;
-              searchReviews();
-            },
-          )
-        ],
-      ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _searchController,
-              decoration: InputDecoration(
-                labelText: 'Search',
-                border: OutlineInputBorder(),
-              ),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: getReviews.length,
-              itemBuilder: (context, index) {
-                final review = getReviews[index];
-                return Card(
-                  child: ListTile(
-                    title: Text(review['reviews']),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('${review['name']}'),
-                        Text('${review['type']}'),
-                       
-                      ],
-                    ),
-                    trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(
-                          icon: Icon(Icons.edit),
-                          onPressed: () {
-                            // go to update page
-                          },
-                        ),
-                        IconButton(
-                          icon: Icon(Icons.delete),
-                          onPressed: () {
-                            // put delete code here
-                          },
-                        ),
-                      ],
-                    ),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _searchController,
+                  decoration: InputDecoration(
+                    labelText: 'Search',
+                    border: OutlineInputBorder(),
                   ),
-                );
-              },
-            ),
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  searchTerm = _searchController.text;
+                  searchReviews();
+                },
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: getReviews.length,
+            itemBuilder: (context, index) {
+              final review = getReviews[index];
+              return Card(
+                child: ListTile(
+                  title: Text(review['reviews']),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('${review['name']}'),
+                      Text('${review['type']}'),
+                    ],
+                  ),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.edit),
+                        onPressed: () {
+                          // go to update page
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () {
+                          // put delete code here
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,17 +1,25 @@
 import 'package:chiwi/components/dashboard/crud_widget.dart';
-import 'package:chiwi/components/stateful/progress_bar_widget.dart';
+import 'package:chiwi/style/colors.dart';
 import 'package:flutter/material.dart';
 
-class DashboardPage extends StatelessWidget{
+class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      appBar: AppBar(
+        title: Text('Reviewers'),
+        backgroundColor: ChiwiColors.CHAI,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [ChiwiColors.VANILLA, ChiwiColors.ALMOND],
+          ),
+        ),
         child: ReviewerDashboard(),
       ),
     );
   }
-
 }
