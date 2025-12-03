771a4c4e99f748c303ac2dda0dfbdac68ac3f2a6
BatteryScentedCandle <aldeasandwich@gmail.com>
2025-12-03 22:29:39 +0800

    modified loading_indicator_widget.dart

--- lib/components/stateless/loading_indicator_widget.dart
+++ lib/components/stateless/loading_indicator_widget.dart
@@ -3,14 +3,15 @@ import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:chiwi/style/colors.dart';
 
 class LoadingIndicator extends StatelessWidget {
-
   const LoadingIndicator({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return LoadingAnimationWidget.staggeredDotsWave(
-      color: ChiwiColors.MATCHA,
-      size: 20,
-    );
+    return Container(
+        child: LoadingAnimationWidget.staggeredDotsWave(
+          color: ChiwiColors.MATCHA,
+          size: 200,
+        ),
+      );
   }
 }
