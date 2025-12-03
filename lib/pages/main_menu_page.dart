51519fa45043680de6ff9293583281a72d1c9283
BatteryScentedCandle <aldeasandwich@gmail.com>
2025-12-03 21:30:10 +0800

    converted the new reviewers button into a sign-out button

--- lib/pages/main_menu_page.dart
+++ lib/pages/main_menu_page.dart
@@ -1,6 +1,8 @@
+import 'package:chiwi/auth/account.dart';
 import 'package:chiwi/components/chiwi/chiwi_widget.dart';
 import 'package:chiwi/components/input/button.dart';
 import 'package:chiwi/pages/dashboard_page.dart';
+import 'package:chiwi/pages/landing_page.dart';
 import 'package:chiwi/pages/templates/two_section_page.dart';
 import 'package:flutter/material.dart';
 
@@ -34,7 +36,16 @@ class MainMenuPage extends StatelessWidget {
                     );
                   },
                 ),
-                Button(text: "New Reviewer", onPressed: () {}),
+                Button(text: "Sign Out", onPressed: () {
+                  AccountManager.INSTANCE.signout();
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LandingPage(),
+                    ),
+                        (route) => false,
+                  );
+                }),
               ],
             ),
           ),
