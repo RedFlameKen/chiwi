3a427d69e8585fd9594be84ad9309aa872b9c31a
RedFlameKen <nettolacaba@gmail.com>
2025-12-04 11:54:31 +0800

    fix: use abi's domain

--- lib/http/server_settings.dart
+++ lib/http/server_settings.dart
@@ -1,5 +1,5 @@
 class ServerSettings {
-  static const host = "thinkpad-x230.taila38b71.ts.net";
+  static const host = "rawr.tailb4d7e5.ts.net";
   static const port = -1;
   static const https = true;
 }
