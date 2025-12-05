a849bf8c4473000a8dfcf7f1bb7fb32abf5aecbc
RedFlameKen <nettolacaba@gmail.com>
2025-12-05 22:30:04 +0800

    fix: use localhost as per requirement

--- lib/http/server_settings.dart
+++ lib/http/server_settings.dart
@@ -1,5 +1,5 @@
 class ServerSettings {
-  static const host = "rawr.tailb4d7e5.ts.net";
-  static const port = -1;
+  static const host = "localhost";
+  static const port = 8080;
   static const https = true;
 }
